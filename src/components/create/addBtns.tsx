474f44301c8c09f5c71a6358b00fc861a4259289
Chaewon <cw06155@gmail.com>
2024-06-05 22:55:04 +0900

    ✨ add question button function

--- src/components/create/addBtns.tsx
+++ src/components/create/addBtns.tsx
@@ -1,7 +1,25 @@
+import { useSurveyStore } from '@/store';
+
 const AddBtns = () => {
+  const { questions, setQuestions } = useSurveyStore();
+  const basicData = {
+    id: questions.length ? Math.max(...questions.map((q) => q.id)) + 1 : 1,
+    type: 'checkbox',
+    title: '',
+    options: [
+      { id: 1, value: '항목 1' },
+      { id: 2, value: '항목 2' },
+    ],
+  };
+  const addQuestion = () => {
+    setQuestions([...questions, basicData]);
+  };
+
   return (
     <div className="border-t-[1px] border-gray-2 flex h-14 items-center">
-      <button className="flex-1 border-r-[1px] border-gray-2">+ 항목 추가</button>
+      <button onClick={addQuestion} className="flex-1 border-r-[1px] border-gray-2">
+        + 항목 추가
+      </button>
       <button className="flex-1">+ 페이지 추가</button>
     </div>
   );
