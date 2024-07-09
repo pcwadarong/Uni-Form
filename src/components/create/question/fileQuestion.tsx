911cac1126c26d0d67cd36d9c885218f0e58c31e
Chaewon <cw06155@gmail.com>
2024-07-09 13:43:37 +0900

    ⚡️ show file on screen, Button disabled depending on file availability

--- src/components/create/question/fileQuestion.tsx
+++ src/components/create/question/fileQuestion.tsx
@@ -6,7 +6,8 @@ import { useState } from 'react';
 import Image from 'next/image';
 
 const FileQuestion: React.FC<QuestionProps> = ({ question, mode }) => {
-  const isDisabled = mode === 'previewing' || mode === 'editing';
+  const [file, setFile] = useState<File | null>(null);
+  const isDisabled = mode === 'previewing' || mode === 'editing' || file !== null;
   const [type, setFileType] = useState('사진');
   const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB
 
@@ -15,10 +16,13 @@ const FileQuestion: React.FC<QuestionProps> = ({ question, mode }) => {
   };
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file && file.size > MAX_FILE_SIZE) {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile && selectedFile.size > MAX_FILE_SIZE) {
       alert('파일 크기가 너무 큽니다. 최대 10MB 이하의 파일을 첨부해주세요.');
       event.target.value = ''; // 선택된 파일 초기화
+      setFile(null);
+    } else {
+      setFile(selectedFile || null);
     }
   };
 
@@ -77,7 +81,7 @@ const FileQuestion: React.FC<QuestionProps> = ({ question, mode }) => {
               width="20"
               height="20"
             />
-            <p className="pl-2">{`${type} 첨부`}</p>
+            <p className={`pl-2 ${isDisabled && 'text-gray-3'}`}>{`${type} 첨부`}</p>
           </label>
           <div>
             <p className="subtitle text-gray-4">{`${type} 업로드는 최대 1개만 가능합니다.`}</p>
@@ -88,6 +92,30 @@ const FileQuestion: React.FC<QuestionProps> = ({ question, mode }) => {
                 : '워드, 엑셀, 파워포인트, 어도비, PDF'
             }`}</p>
           </div>
+          {file && (
+            <div className="mt-4">
+              {type === '사진' ? (
+                <div className="relative w-fit">
+                  <button
+                    onClick={() => setFile(null)}
+                    style={{ lineHeight: '1.3rem' }}
+                    className="absolute right-3 top-3 w-6 h-6 bg-dark/50 rounded-full text-white flex justify-center"
+                  >
+                    x
+                  </button>
+                  <Image
+                    src={URL.createObjectURL(file)}
+                    alt="첨부된 이미지"
+                    width="150"
+                    height="150"
+                    className="rounded-lg"
+                  />
+                </div>
+              ) : (
+                <p>{file.name}</p>
+              )}
+            </div>
+          )}
         </>
       )}
     </div>
