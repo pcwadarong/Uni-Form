6b871a2351ba3d8e0ba94577e9ce8f91dcbe121d
Chaewon <cw06155@gmail.com>
2024-07-25 20:33:50 +0900

    🚧 modify route

--- src/app/analyze/[...query]/page.tsx
+++ src/app/analyze/[...query]/page.tsx
@@ -13,7 +13,20 @@ const AnalyzePage = () => {
   const pathname = usePathname();
   const encryptedId = pathname.replace('/analyze/', '');
   const itemId = encryptedId ? decrypt(encryptedId) : '';
-  return <div>Analyze Page for Item ID: {itemId}</div>;
+  return (
+    <>
+      <div>분석페이지 for Item ID: {itemId}</div>
+      <p>분석 예정입니다.</p>
+      <p>작업중!</p>
+      <div>
+        <p>댓글 목록</p>
+        <div>
+          <input type="text" name="" id="" />
+          <button>댓글 달기</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AnalyzePage;
