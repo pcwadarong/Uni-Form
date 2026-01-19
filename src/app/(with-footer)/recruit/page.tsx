83407d710fbfa41631910ad88cf79c444d1ce927
Chaewon <pcwadarong@naver.com>
2026-01-20 04:16:13 +0900

    fix: searchParams 비동기화

--- src/app/(with-footer)/recruit/page.tsx
+++ src/app/(with-footer)/recruit/page.tsx
@@ -4,16 +4,17 @@ import { fetchFormList } from "@/lib/firebase/form/getFormListServer";
 export default async function ListServerWrapper({
   searchParams,
 }: {
-  searchParams: { cat?: string; sort?: string };
+  searchParams: Promise<{ cat?: string; sort?: string }>;
 }) {
+  const { cat, sort } = await searchParams;
   const initialData = await fetchFormList("recruit", "public");
 
   return (
     <ListClient
       initialData={initialData}
       topic="recruit"
-      category={searchParams.cat ?? "all"}
-      sort={searchParams.sort ?? "random"}
+      category={cat ?? "all"}
+      sort={sort ?? "random"}
     />
   );
 }
